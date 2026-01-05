c3818056af21cb57b0e08f89118956982e3e091a
Luca-Paul Schirmer <lucapaulschirmer@gmail.com>
2026-01-05 22:22:26 +0100

    Neue Methoden: Finance KPI's berechnen und Activity Durations bei Event Log mit lifcycle:transition Spalte. Doku zu anderen Methoden hinzugefügt

--- backend/process_analysis_engine/analysis_utils.py
+++ backend/process_analysis_engine/analysis_utils.py
@@ -2,44 +2,51 @@ import pandas as pd
 import numpy as np
 import pm4py
 
-# Information that describes the basic structure of a given event log and its underlying process, relevant for all agents
 def get_basic_information(event_log: pd.DataFrame) -> dict:
+
+    """Returns a dictionary with basic information about the event log and the underlying process."""
+
     return {
         "number_cases" : event_log["case:concept:name"].nunique(),
-        
         "variants_frequency" : pm4py.get_variants(event_log),
         "number_variants" : len(pm4py.get_variants(event_log)),
-
         "number_events" : len(event_log),
-
         "activities" : list(event_log["concept:name"].unique()),
         "number_activities" : event_log["concept:name"].nunique(),
         "activities_frequency" : event_log["concept:name"].value_counts().to_dict(),
         "start_activities" : pm4py.get_start_activities(event_log),
         "end_activities" : pm4py.get_end_activities(event_log),
-
         "number_resources" : event_log["org:resource"].nunique(),
         "resources" : list(event_log["org:resource"].unique())
     }
 
-# KPI's for Performance Agent
-# Case Durations in hours, works for Datasets with 1 Timestamp Column
 def get_case_durations(event_log: pd.DataFrame) -> pd.DataFrame:
+
+    """Returns a DataFrame with case durations in hours for each case. Works for datasets with one timestamp column."""
+
     return (event_log.groupby("case:concept:name")["time:timestamp"]
             .agg(lambda x: ((x.max() - x.min()).total_seconds())/3600)
             .reset_index(name="case_duration_hours")
     )
 
-def get_case_duration_mean_var_std(event_log: pd.DataFrame) -> dict:
+def get_case_duration_statistics(event_log: pd.DataFrame) -> dict:
+
+    """Returns a dictionary with descriptive statistics about case durations in hours."""
+
     case_durations = get_case_durations(event_log)
     return {
+        "min": np.min(case_durations["case_duration_hours"]).round(2),
+        "max": np.max(case_durations["case_duration_hours"]).round(2),
+        "median": np.median(case_durations["case_duration_hours"]).round(2),
         "mean": np.mean(case_durations["case_duration_hours"]).round(2),
         "variance": np.var(case_durations["case_duration_hours"]).round(2), 
         "standard_deviation": np.std(case_durations["case_duration_hours"]).round(2)
     }
 
-# Variant durations in hours (overall & mean) with its respective frequency in event log
 def get_variant_durations_frequency(event_log: pd.DataFrame) -> pd.DataFrame:
+
+    """Returns a DataFrame with variant durations in hours (overall & mean) along with their respective frequency in the event log."""
+
     variants = get_basic_information(event_log)["variants_frequency"]
     variants_df = pd.DataFrame([
         {"@@variant_column":x, "frequency":y} for x, y in variants.items()
@@ -53,10 +60,40 @@ def get_variant_durations_frequency(event_log: pd.DataFrame) -> pd.DataFrame:
     return variants_durations_frequency
 
 # Activity duratations in hours (overall & mean) with its respective frequency in event log
-def get_activity_duration(event_log: pd.DataFrame, end_time: bool = False) -> pd.DataFrame:
-    if end_time == True:
-        pass # ToDo: Write case if end_time == True
-    # Ending activities are not analyzed
+def get_activity_duration(event_log: pd.DataFrame) -> pd.DataFrame:
+    if "lifecycle:transition" in event_log.columns:
+        event_log.sort_values(["case:concept:name", "concept:name", "time:timestamp"], inplace=True)
+        event_log["occurrence"] = (event_log.groupby(["case:concept:name", "concept:name", "lifecycle:transition"]).cumcount())
+        start_activities = event_log[event_log["lifecycle:transition"] == "start"]
+        end_activities = event_log[event_log["lifecycle:transition"] == "complete"]
+        merged_activities = pd.merge(
+            start_activities,
+            end_activities,
+            on=[
+                "case:concept:name",
+                "concept:name",
+                "occurrence"
+                ],
+                suffixes=("_start", "_end"),
+                how="inner"
+                )
+
+        merged_activities["duration_seconds"] = (
+            (merged_activities["time:timestamp_end"] - merged_activities["time:timestamp_start"]).dt.total_seconds())
+
+        merged_activities = merged_activities[
+            [
+        "case:concept:name",
+        "concept:name",
+        "duration_seconds",
+        "time:timestamp_start",
+        "time:timestamp_end"
+        ]
+        ]
+
+        result = (merged_activities.groupby("concept:name")["duration_seconds"].sum()/(60*60)).round(2).reset_index(name="overall_activity_duration_hours")
+        result["mean_activity_duration_hours"] = (merged_activities.groupby("concept:name")["duration_seconds"].mean()/(60*60)).round(2).values
+        return result
     else:
         activities = get_basic_information(event_log)["activities_frequency"]
         activities_df = pd.DataFrame([
@@ -72,24 +109,44 @@ def get_activity_duration(event_log: pd.DataFrame, end_time: bool = False) -> pd
         df = pd.merge(activities_df, activities_durations_frequency_agg, on="concept:name", how="inner")
         df["mean_activity_duration_hours"] = (df["overall_activity_duration_hours"]/df["frequency"]).round(2)
         return df
+    
+def get_waiting_times(event_log: pd.DataFrame) -> pd.DataFrame:
+    pass
 
-# KPI's for Finance Agent
-# Costs per Case, Cost column needs to be of type Integer
 def get_costs_per_case(event_log: pd.DataFrame) -> pd.DataFrame:
+
+    """Returns a DataFrame with total costs per case."""
+
     return event_log.groupby("case:concept:name")["cost:amount"].sum().reset_index()
 
-# Costs per Variant
+def get_case_costs_statistics(event_log: pd.DataFrame) -> dict:
+
+    """Returns a dictionary with descriptive statistics about case costs."""
+
+    case_costs = get_costs_per_case(event_log)
+    return {
+        "min": np.min(case_costs["cost:amount"]).round(2),
+        "max": np.max(case_costs["cost:amount"]).round(2),
+        "median": np.median(case_costs["cost:amount"]).round(2),
+        "mean": np.mean(case_costs["cost:amount"]).round(2),
+        "variance": np.var(case_costs["cost:amount"]).round(2), 
+        "standard_deviation": np.std(case_costs["cost:amount"]).round(2)
+    }
+
+
 def get_costs_per_variant(event_log: pd.DataFrame) -> pd.DataFrame:
     pass
 
-# Costs per Activity
+
 def get_costs_per_activity(event_log: pd.DataFrame) -> pd.DataFrame:
+
+    """Returns a DataFrame with total and mean costs per activity."""
+
     return (event_log.groupby("concept:name")
                           .agg(total_costs = ("cost:amount", "sum"),
                                mean_costs = ("cost:amount", "mean"))
                                .reset_index())
 
-# KPI's for Risk Agent
 def get_rework_stats(event_log: pd.DataFrame) -> dict:
     rework_cases_per_activity = pm4py.get_rework_cases_per_activity(event_log)
     for i in range(get_basic_information(event_log)["number_activities"]):
@@ -97,18 +154,24 @@ def get_rework_stats(event_log: pd.DataFrame) -> dict:
             rework_cases_per_activity[get_basic_information(event_log)["activities"][i]] = 0
     return rework_cases_per_activity
 
-# KPI's for Compliance Agent
-# All process paths with its corresponding variant
 def get_process_paths(event_log: pd.DataFrame) -> pd.DataFrame:
+    """Returns a DataFrame with all process paths along with their corresponding variant."""
+
     return pm4py.get_variants_paths_duration(event_log)[["concept:name", "concept:name_2", "@@variant_column"]].reset_index(drop=True)
 
 def get_activities_per_resources(event_log: pd.DataFrame):
+    """Returns a DataFrame with activities per resource."""
+
     return event_log.groupby("org:resource")["concept:name"].agg(list).reset_index()
 
 def get_resources_per_activities(event_log: pd.DataFrame) -> pd.DataFrame:
+    """Returns a DataFrame with resources per activity."""
+
     return event_log.groupby("concept:name")["org:resource"].agg(list).reset_index()
 
 def make_json_serializable(obj) -> None:
+    """Recursively makes an object JSON serializable by converting non-serializable types."""
+
     if isinstance(obj, dict):
         new_dict = {}
         for key, value in obj.items():
--- backend/process_analysis_engine/analysis_workflow.py
+++ backend/process_analysis_engine/analysis_workflow.py
@@ -11,7 +11,7 @@ def calculate_result_dict_performance(event_log: pd.DataFrame) -> str:
     dict = {
     "process_paths_with_its_variants":get_process_paths(event_log).to_dict(orient="records"),
     "case_durations":get_case_durations(event_log).to_dict(orient="records"),
-    "case_durations_variance_standard_deviation":get_case_duration_mean_var_std(event_log),
+    "case_durations_statistics":get_case_duration_statistics(event_log),
     "variants_frequency_total_mean_durations":get_variant_durations_frequency(event_log).to_dict(orient="records"),
     "activities_frequency_total_mean_durations":get_activity_duration(event_log).to_dict(orient="records"),
     "rework_cases_per_activity":get_rework_stats(event_log),
@@ -27,6 +27,7 @@ def calculate_result_dict_finance(event_log: pd.DataFrame) -> str:
     dict = {
     "process_paths_with_its_variants":get_process_paths(event_log).to_dict(orient="records"),
     "total_costs_per_case":get_costs_per_case(event_log).to_dict(orient="records"),
+    "case_costs_statistics":get_case_costs_statistics(event_log),
     "total_mean_costs_per_activity":get_costs_per_activity(event_log).to_dict(orient="records"),
     "rework_cases_per_activity":get_rework_stats(event_log),
     "activities_per_resources":get_activities_per_resources(event_log).to_dict(orient="records"),
